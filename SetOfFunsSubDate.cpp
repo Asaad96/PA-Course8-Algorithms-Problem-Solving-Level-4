5e03e5e715bc944e9000cab8ec5755aea98ea6c0
Asaad Baz <Asaad.baz96@gmail.com>
2026-06-10 18:56:45 +0800

    Decrease (one or X) days

---
+++ SetOfFunsSubDate.cpp
@@ -0,0 +1,35 @@
+
+#include "my_library.h"
+#include <iostream>
+#include <cerrno>
+using namespace std;
+
+stDate DecreaseDateByXDay(stDate Date, short XDay)
+{
+    if(XDay == 0)
+    {return Date;}
+
+    
+     for (short i = 0 ; i < XDay ; i++)
+        {
+            Date = DecreaseDateOneDay(Date);
+        }
+
+    
+
+    return Date;
+}
+
+
+
+int main ()
+{
+    stDate Date = ReadFullDate();
+    short XDay = ReadNumber("\nHow many Days?\t");
+    Date = DecreaseDateOneDay(Date);
+  cout << "\nDecreasing Date One Day\t"<< Date.Day << "." << Date.Month << "." << Date.Year;
+Date = DecreaseDateByXDay(Date, XDay);
+cout << "\nDecreasing Date " << XDay << " Days "<< Date.Day << "." << Date.Month << "." << Date.Year;
+return 0;
+}
+
